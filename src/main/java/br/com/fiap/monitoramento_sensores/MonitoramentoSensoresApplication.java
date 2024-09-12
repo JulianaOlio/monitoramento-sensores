d9f4ad9754fda92263abdb828dad1753947a9f38
Juliana <jullymary@hotmail.com>
2024-09-11 23:44:52 -0300

    criação projeto monitoramento de sensores via spring initializr

---
+++ src/main/java/br/com/fiap/monitoramento_sensores/MonitoramentoSensoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.monitoramento_sensores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoSensoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoramentoSensoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/monitoramento_sensores/MonitoramentoSensoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.monitoramento_sensores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoSensoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
